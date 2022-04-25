93370fe07de6e1c1cfb1d90bbe97239d57183686
windows10 <windows10@DESKTOP-3A0C53F>
2022-04-24 21:51:11 -0300

    punto16-tp2

---
+++ TP2_Grupo27/src/main/java/ar/edu/unju/fi/ejercicio16/Auxiliar.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio16;
+
+public class Auxiliar {
+
+	int iNumero = 5;
+	int iSumatorio = 0;
+	int iContador = iNumero;
+	
+	
+	public int getiNumero() {
+		return iNumero;
+	}
+	public void setiNumero(int iNumero) {
+		this.iNumero = iNumero;
+	}
+	public int getiSumatorio() {
+		return iSumatorio;
+	}
+	public void setiSumatorio(int iSumatorio) {
+		this.iSumatorio = iSumatorio;
+	}
+	public int getiContador() {
+		return iContador;
+	}
+	public void setiContador(int iContador) {
+		this.iContador = iContador;
+	}
+}
---
+++ TP2_Grupo27/src/main/java/ar/edu/unju/fi/ejercicio16/CalculoMatematico.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio16;
+
+public class CalculoMatematico {
+
+	
+	public static void main(String[] args) {
+		
+		
+				
+		while (iContador != 0) {
+				   
+		  iSumatorio = iSumatorio + iContador;
+		  iContador--;
+			 
+		}
+				
+		System.out.println("El sumatorio de " + iNumero + " es " + iSumatorio);
+		
+		
+	}
+}
